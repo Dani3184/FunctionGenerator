47d12dc00450ab3ce72ac6f3c34f127d211c7a4c
Dani_318 <danielrgm222@gmail.com>
2026-04-23 23:15:32 -0400

    Refactor testbench with enhanced waveform tests
    Updated waveform test cases and added comments for clarity.

--- test/tb.v
+++ test/tb.v
@@ -7,7 +7,6 @@ reg rst_n = 0;
 reg [7:0] ui_in = 0;
 wire [7:0] uo_out;
 
-// DUT
 tt_um_gen_onda uut (
     .clk(clk),
     .rst_n(rst_n),
@@ -19,7 +18,6 @@ tt_um_gen_onda uut (
     .uio_oe()
 );
 
-// Clock 100 MHz
 always #5 clk = ~clk;
 
 initial begin
@@ -32,24 +30,26 @@ initial begin
     #20;
     rst_n = 1;
 
-    // Waveforms
-    ui_in = 8'b000_100_01; #5000;
-    ui_in = 8'b001_111_10; #5000;
-    ui_in = 8'b010_111_11; #5000;
-    ui_in = 8'b011_011_10; #5000;
-    ui_in = 8'b100_111_01; #5000;
+    // --- Waveforms Test ---
+    // Format: 8'b[Freq][Amp][Func]
+    ui_in = 8'b00_100_000; #5000; // Sine, Amp 100, Freq 00
+    ui_in = 8'b00_100_001; #5000; // Saw, Amp 100, Freq 00
+    ui_in = 8'b00_100_010; #5000; // Square, Amp 100, Freq 00
+    ui_in = 8'b00_100_011; #5000; // Triangle, Amp 100, Freq 00
+    ui_in = 8'b00_100_100; #5000; // Quadratic, Amp 100, Freq 00
 
-    // Frequency sweep
-    ui_in[5:0] = 6'b000100;
+    // --- Frequency sweep ---
+    ui_in[2:0] = 3'b001; // Fix to Sawtooth
+    ui_in[5:3] = 3'b100; // Fix Amplitude
 
     ui_in[7:6] = 2'b00; #4000;
     ui_in[7:6] = 2'b01; #4000;
     ui_in[7:6] = 2'b10; #4000;
     ui_in[7:6] = 2'b11; #4000;
 
-    // Amplitude sweep
-    ui_in[7:6] = 2'b01;
-    ui_in[2:0] = 3'b000;
+    // --- Amplitude sweep ---
+    ui_in[7:6] = 2'b01; // Fix Frequency
+    ui_in[2:0] = 3'b000; // Fix to Sine
 
     ui_in[5:3] = 3'b001; #3000;
     ui_in[5:3] = 3'b010; #3000;
@@ -60,5 +60,3 @@ initial begin
 end
 
 endmodule
-
-
